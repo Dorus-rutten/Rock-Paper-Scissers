4a9e0cd019b879175bfad035f6ca173e024e8220
Dorus <dorusrutten@gmail.com>
2024-11-20 16:15:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,6 @@ setup(
     author="Dorus Rutten",
     author_email="Dorsrutten@gmail.com",
     description="A AI that plays Rock Papier Scissers with u using the camera",
-    long_description=open("README.md").read(),  # Reads your README.md for details
-    long_description_content_type="text/markdown",
     url="https://github.com/Dorus-rutten/Rock-Paper-Scissers",  # Replace with your GitHub repo
     packages=find_packages(),  # Automatically finds your Python packages
     install_requires=[
